fd8bc9fe765598b62b3abbb8437bbf534d3dff02
Sean Derrow <sderrow@mit.edu>
2017-11-10 18:12:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function takeANumber(katzDeliLine, newName) {
   katzDeliLine.push(newName)
-  return `Welcome, ${newName}. You are number ${katzDeliLine.length + 1} in line.`
+  return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-    return katzDeliLine.shift()
+    return `Currently serving ${katzDeliLine.shift()}`
   } else {
     return "There is nobody waiting to be served!"
   }
